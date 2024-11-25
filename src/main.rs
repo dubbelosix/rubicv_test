0d6bcf846352ec0714ba83bfbbbd3d8d6cf1a686
dubbelosix <dub@006.com>
2024-11-26 00:56:11 +0530

    fix some issues

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,39 @@ use core::panic::PanicInfo;
 
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
-    loop {}
+    unsafe {
+        core::arch::asm!(
+        "ecall",
+        options(noreturn, nomem, nostack)
+        );
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    let x = 42;
-    let y = 27;
-    let result = x + y;
-    
+    // Fetching args
+    let arg_count: u32;
     unsafe {
-        core::arch::asm!("mv a0, {}", in(reg) result);
-        core::arch::asm!("ebreak");
+        core::arch::asm!("mv {}, a0", out(reg) arg_count);
     }
-    
-    loop {}
+    let args_ptr: *const u32 = 0xA000_0000 as *const u32;
+    let args = unsafe { core::slice::from_raw_parts(args_ptr, arg_count as usize) };
+
+    // Code starts here
+    let x = args[0];
+    let y = args[1];
+    let result = x + y;
+    // Code ends here
+
+    // Return
+    unsafe {
+        core::arch::asm!(
+        "mv a0, {0}",
+        "ecall",
+        in(reg) result,
+        options(noreturn, nomem, nostack)
+        );
+    }
+
 }
+
